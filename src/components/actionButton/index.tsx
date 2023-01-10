15d68d2d6f23ef2c883a8b040a46b26536476a35
Wail Solaiman <wailsolaiman@hotmail.com>
2023-01-10 14:06:37 +0100

    add new component for global buttons

---
+++ src/components/actionButton/index.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+
+import { SelectedPage } from "@/shared/types";
+
+type ActionButtonProps = {
+  children: ReactNode;
+  setSelectedPage: (value: SelectedPage) => void;
+};
+
+const ActionButton = ({ children, setSelectedPage }: ActionButtonProps) => {
+  return (
+    <AnchorLink
+      className="rounded-md bg-secondary-500 px-10 py-2 hover:bg-primary-500 hover:text-white "
+      onClick={() => setSelectedPage(SelectedPage.ContactUs)}
+      href={`#${SelectedPage.ContactUs}`}
+    >
+      {children}
+    </AnchorLink>
+  );
+};
+
+export default ActionButton;
